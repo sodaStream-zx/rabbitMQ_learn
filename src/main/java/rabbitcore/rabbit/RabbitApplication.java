70dbeee6299cd6a81e2de498c69fd8adcb556845
riceincoke <42928623+riceincoke@users.noreply.github.com>
2019-05-08 00:12:56 +0800

    延迟队列实现

--- src/main/java/rabbitcore/rabbit/RabbitApplication.java
+++ src/main/java/rabbitcore/rabbit/RabbitApplication.java
@@ -2,28 +2,27 @@ package rabbitcore.rabbit;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import rabbitcore.rabbit.TimeOutQueue.ProProducer;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
 @EnableRabbit
-public class RabbitApplication {
+public class RabbitApplication implements ApplicationRunner {
 
 //    @Autowired
 //    private SpringProducer springProducer;
 
     @Autowired
     private ProProducer proProducer;
-
     public static void main(String[] args) {
         SpringApplication.run(RabbitApplication.class, args);
     }
 
-    @PostConstruct
-    public void show() {
+    @Override
+    public void run(ApplicationArguments args) {
         // springProducer.sendMessage();
         //springProducer.reciveMessage();
         proProducer.sendMessage();
